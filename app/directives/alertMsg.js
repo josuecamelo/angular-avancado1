ff700d537257472e7d8e584d579d0a119ca0de11
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-20 12:46:15 -0200

    Componentes com Diretivas - Possue Erros

--- app/directives/alertMsg.js
+++ app/directives/alertMsg.js
@@ -1,21 +1,13 @@
-/**
- * Created by josuecamelo on 20/12/17.
- */
 module.exports = function(){
     return {
-        /*template:  `
-            <div class="alert alert-success text-center">
-                <p>a</p>
-            </div>
-        `,*/
         template:
         `<div class="alert alert-success text-center">
-            <p>{{ title }} <b ng-transclude></b></p>
-        </div>`,
+                <p>a</p>
+            </div>`,
         replace: false,
         restrict: 'AE',
         scope:{
-            title: '@' //recebe um titulo
+            title: '@',
         },
         transclude: true
     };
