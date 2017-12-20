4ef5abd211e3f8daa0ce5139519e17822670433e
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-20 14:51:27 -0200

    Rotas para 1

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 require('angular');
-
+require('angular-route'); //ng route
 require('./locale/angular-locale_pt-br.js');
 
+var routeConfig = require('./config/routeConfig');
 var configConstant = require('./config/configConstant');
 var configValue = require('./config/configValue');
 var configBonusProvider = require('./config/configBonusProvider');
@@ -12,16 +13,20 @@ var MainController = require('./controllers/MainController');
 var maskTel = require('./directives/maskTel');
 //var alertMsg = require('./directives/alertMsg');
 
-angular.module('app', []);
+//angular.module('app', []);
+angular.module('app',['ngRoute']); //adicionado o ngroute
 angular.module('app').constant('configConstant',configValue);
 angular.module('app').value('configValue',configValue);
 angular.module('app').provider('bonusGenerator',[bonusGenerator]);
 //angular.module('app').config(['bonusGeneratorProvider',configBonusProvider]);
 angular.module('app').config(['bonusGeneratorProvider','configConstant',configBonusProvider]);
+
+angular.module('app').config(['$routeProvider',routeConfig]);
+
 //angular.module('app').factory('clientAPIService',['$http',clientAPIService]);
 //angular.module('app').service('clientTestService',['$http',clientTestService]);
 angular.module('app').factory('clientAPIService',['$http','configValue',clientAPIService]); //usando service value
 angular.module('app').service('clientTestService',['$http','configValue',clientTestService]); //usando service value
 angular.module('app').directive('maskTel',[maskTel]); //registrado directive
 //angular.module('app').directive('alertMsg',[alertMsg]); //registrado directive
-angular.module('app').controller('MainController',['$scope','$http', '$filter', 'clientAPIService','clientTestService', 'configValue', 'bonusGenerator',MainController]);
+angular.module('app').controller('MainController',['$scope','$http', '$filter', 'clientAPIService','clientTestService', 'configValue', 'bonusGenerator', 'routeName',MainController]);
--- app/config/routeConfig.js
+++ app/config/routeConfig.js
@@ -1,3 +1,14 @@
 /**
  * Created by josuecamelo on 20/12/17.
  */
+module.exports = function($routeProvider){
+    $routeProvider.when("/clients",{
+        templateUrl:"view/clients.html",
+        controller:"MainController",
+        resolve:{
+            routeName:function(){
+                return "Client list";
+            }
+        }
+    });
+};
